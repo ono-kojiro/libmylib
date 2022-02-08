37d64a41a0edfb86a7d8996aa124bf5c9879d829
Kojiro ONO <ono.kojiro@gmail.com>
2022-02-08 19:50:32 +0900

    add build scripts

---
+++ src/mylib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "mylib.h"
+
+int mylib(void)
+{
+  printf("this is mylib.\n");
+  return 0;
+}
+
+int mylib_add(int a, int b)
+{
+  return a + b;
+}
+
---
+++ src/mylib.h
@@ -0,0 +1,9 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+int mylib(void);
+
+int mylib_add(int a, int b);
+
+#endif /* #ifndef MYLIB_H */
+
